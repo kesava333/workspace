24b2299b0180421e273374a743927243b46f4f09
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2022-10-26 15:15:21 -0400

    Delete smtp.py

--- smtp.py
+++
@@ -1,6 +0,0 @@
-import smtplib
-host = "smtp.clearpath.ai"
-server = smtplib.SMTP(host)
-msg = "Hello! This Message is to test deadman switch"
-server.sendmail("bundles-noreply@clearpath.ai", "kginjupalli@clearpath.ai", msg)
-server.quit()
