e34c6a21409d02bfa5a6678b920acf347adbf1c7
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2022-11-03 22:53:42 +0530

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -2,6 +2,8 @@
   from email.mime.multipart import MIMEMultipart
   from email.mime.text import MIMEText
   
+  senderItems=['i-057778eda2f071c8e ==> aws-fs-kesava-01-rb-1', 'i-0189d6765094b9c83 ==> aws-fs-kesava-01-gz', 'i-0291bf04895773a8e ==> aws-fs-kesava-01-fm']
+  
   mail_content = '''Hello,
   This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
   Thank You '''+'''\n'''+'\n'.join(map(str,senderItems))
