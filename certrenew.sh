f0f6b367fb753d75a4ce419dc4330d134edcb84b
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2022-12-06 00:19:02 +0530

    Create certrenew.sh

---
+++ certrenew.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/bin/aws s3 cp s3://otto-motors/aws.ottomotors.com/privkey.pem  /etc/ssl/private/aws.ottomotors.com.key
+/usr/bin/aws s3 cp s3://otto-motors/aws.ottomotors.com/cert.pem /etc/ssl/certs/aws.ottomotors.pem
+
+/etc/init.d/nginx restart
