5cfb77d5398f87c15f3e1c78de9e3717073382b1
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2023-01-04 21:42:31 +0530

    Update eip.py

--- eip.py
+++ eip.py
@@ -20,7 +20,7 @@ for AWSRegion in RegionList:
 
     addresses_dict = ec2.describe_addresses()
     for eip_dict in addresses_dict['Addresses']:
-        if "InstanceId" not in eip_dict:
+        if "AssociationId" not in eip_dict:
             print (eip_dict['PublicIp'] +
                 " doesn't have any instances associated, releasing")
             ec2.release_address(AllocationId=eip_dict['AllocationId'])
