aa41f5dbc6e589af5f0a5733a5bfbfb0ada1d3ff
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2023-01-18 08:59:22 +0530

    Update runcommand.py

--- runcommand.py
+++ runcommand.py
@@ -1,10 +1,15 @@
 import json
+import paramiko
+import boto3
 
-sshUsername="ec2-user"
-sshKeyPath="/home/ec2-user/WEBSERVER.pem"
+sshKeyPath="WEBSERVER.pem"
+scriptName="bash.sh"
+sshUser='ec-user'
 
-def lambda_handler(event, context):                       #Start of our function
-    instance = ec2.create_instances(
+
+def lambda_handler(event, context):
+    
+        instance = ec2.create_instances(
         InstanceType=INSTANCE_TYPE,
         KeyName=KEY_NAME,
         ImageId=AMI,
@@ -22,7 +27,8 @@ def lambda_handler(event, context):                       #Start of our function
     )
     print("New instance created:", instance[0].id)
     
-    
+    shell_output = open(scriptName, "r")
+    op=(shell_output.read())
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     
@@ -34,18 +40,21 @@ def lambda_handler(event, context):                       #Start of our function
         try:
             retries += 1
             print('SSH into the instance: {}'.format(ip_address))
-            ssh.connect(hostname=ip_address, username='ec2-user', pkey=privkey)
+            ssh.connect(hostname=ip_address, username=sshUser, pkey=privkey)
             return True
         except Exception as e:
-            print(e)
+            
             time.sleep(interval)
             print('Retrying SSH connection to {}'.format(ip_address))
             ssh_connect_with_retry(ssh, ip_address, retries)
     
     
-    
-    ssh_connect_with_retry(ssh, instance[0].id, 0)
-    
-    stdin, stdout, stderr = ssh.exec_command("sudo bash bash.sh")
-    print('stdout:', stdout.read())
-    print('stderr:', stderr.read())
+
+    ssh_connect_with_retry(ssh, instance[0].id, 3)
+
+    ftp_client=ssh.open_sftp()
+    ftp_client.put(scriptName,scriptName)
+    ftp_client.close()
+    stdin, stdout, stderr = ssh.exec_command("sudo bash ./"+scriptName)
+    print('Shell Output:', stdout.read())
+    print('SSH Errors:', stderr.read())
