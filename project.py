42cb2fa22b3a9bba6b994f1ef42b92e0ff0c204d
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2023-01-18 23:04:31 +0530

    Update project.py

--- project.py
+++ project.py
@@ -32,3 +32,44 @@ def lambda_handler(event, context):
         print('Ingress Successfully Set %s' % data)
     except ClientError as e:
         print(e)
+----
+
+
+
+
+import json
+import boto3
+
+
+prefix_list_cidr_blocks = ['10.0.0.0/24', '192.168.1.0/24', '172.16.0.0/16']
+security_group_name="test1"
+security_group_description="test1"
+ingress_sg=[
+        {'IpProtocol': 'tcp', 'FromPort': 22, 'ToPort': 22, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+        {'IpProtocol': 'tcp', 'FromPort': 80, 'ToPort': 80, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+    ]
+prefix_list_name="demo1"
+vpc_id=""
+
+def lambda_handler(event, context):
+    # TODO implement
+    # Create a client for the EC2 service
+    ec2 = boto3.client('ec2')
+    try:
+        # Create a security group
+        response = ec2.create_security_group(
+            GroupName=security_group_name,
+            Description=security_group_description,
+             VpcId=vpc_id
+        )
+
+        # Get the security group ID
+        security_group_id = response['GroupId']
+
+        # Authorize inbound traffic for SSH and HTTP
+        ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpPermissions=ingress_sg
+        )
+     except ClientError as e:
+        print(e)
