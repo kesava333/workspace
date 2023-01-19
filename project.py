590c99a66f208a10fd61959ad6ec4ff8be872bbd
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-01-19 13:37:19 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,124 +1,78 @@
 import json
 import boto3
+import sys
 from botocore.exceptions import ClientError
+#SgName = sys.argv[1]
+Project_Name = sys.argv[1]
+#PlName = sys.argv[2]
+Plentries = sys.argv[2]
+ec2 = boto3.client('ec2',region_name = 'us-east-2')
 
-def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    
-    response = ec2.describe_vpcs()
-    vpc_id = response.get('Vpcs', [{}])[1].get('VpcId', '')
-    
-    try:
-        response = ec2.create_security_group(GroupName='Kesava-Test',
-                                             Description='kesava-test',
-                                             VpcId=vpc_id)
-        response = ec2.create-managed-prefix-list(prefix-list-name='Kesava-Test',
-                                             Description='kesava-test')                                     
-        security_group_id = response['GroupId']
-        print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
-    
-        data = ec2.authorize_security_group_ingress(
-            GroupId=security_group_id,
-            IpPermissions=[
-                {'IpProtocol': 'tcp',
-                 'FromPort': 80,
-                 'ToPort': 80,
-                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-                {'IpProtocol': 'tcp',
-                 'FromPort': 22,
-                 'ToPort': 22,
-                 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-            ])
-        print('Ingress Successfully Set %s' % data)
-        ec2.create_tags(
-        Resources=[security_group_id],
-        Tags=[
-            {
-                'Key': 'Name',
-                'Value': 'My Security Group'
-            },
-            {
-                'Key': 'Environment',
-                'Value': 'Production'
-            }
-        ]
-    )
-    except ClientError as e:
-        print(e)
-----
-import json
-import boto3
+response = ec2.describe_vpcs()
+vpc_id = response.get('Vpcs', [{}])[1].get('VpcId', '')
 
-
-
-security_group_name="test1"
-security_group_description="test1"
-ingress_sg=[
-        {'IpProtocol': 'tcp', 'FromPort': 22, 'ToPort': 22, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
-        {'IpProtocol': 'tcp', 'FromPort': 80, 'ToPort': 80, 'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
-    ]
-prefix_list_name="demo1"
-prefixListEntry=[
+try:
+    print("Creating Prefix List:")
+    prefix_list_name=Project_Name+'-Pl'
+    prefixListEntry=[
         {
-            'Cidr': '192.0.2.0/24',
+            'Cidr': Plentries,
             'Description': 'Test entry 1'
-        },
-        {
-            'Cidr': '198.51.100.0/24',
-            'Description': 'Test entry 2'
         }
     ]
-    
-vpc_id="vpcid"
-
-def lambda_handler(event, context):
-    # TODO implement
-    # Create a client for the EC2 service
-
-    ec2 = boto3.client('ec2')
-    try:
-        # Create a security group
-        response = ec2.create_security_group(
-            GroupName=security_group_name,
-            Description=security_group_description,
-             VpcId=vpc_id
-        )
-
-        # Get the security group ID
-        security_group_id = response['GroupId']
-
-
-        ec2.authorize_security_group_ingress(
-            GroupId=security_group_id,
-            IpPermissions=ingress_sg
-        )
-     except ClientError as e:
-        print(e)
-
-    
     prefix_list_entries = prefixListEntry
-    
 
     prefixList = ec2.create_managed_prefix_list(
     DryRun=False,
     PrefixListName=prefix_list_name,
     Entries=prefix_list_entries,
-    MaxEntries=100,
+    MaxEntries=5,
     AddressFamily='IPv4'
     )
     
     prefixListID=prefixList['PrefixList']['PrefixListId']
+    print("Prefix List ID:" +prefixListID)
     
-    
-    -----------------------------
-    instance_name_tag = Project_Name
+    response = ec2.create_security_group(GroupName=Project_Name+'-sg',
+                                            Description=Project_Name+'-sg',
+                                            VpcId=vpc_id)
+                                    
+    security_group_id = response['GroupId']
+    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+
+    data = ec2.authorize_security_group_ingress(
+        GroupId=security_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+                'FromPort': 80,
+                'ToPort': 80,
+                'PrefixListIds': [{'PrefixListId': prefixListID}]},
+            {'IpProtocol': 'tcp',
+                'FromPort': 443,
+                'ToPort': 443,
+                'PrefixListIds': [{'PrefixListId': prefixListID}]}
+        ])
+    print('Ingress Successfully Set %s' % data)
+    ec2.create_tags(
+        Resources=[security_group_id],
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': Project_Name+'-sg'
+            },
+            {
+                'Key': 'Environment',
+                'Value': 'Simulation-Deploy'
+            }
+        ]
+    )
+
+   instance_name_tag = Project_Name
    instances = ec2.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [instance_name_tag]}])
    instance_id = response['Reservations'][0]['Instances'][0]['InstanceId']
    response = ec2.modify_instance_attribute(
                 InstanceId=instance_id,
                 Groups=[security_group_id]
             )
-
-    
-    
-    
+except ClientError as e:
+    print(e)
