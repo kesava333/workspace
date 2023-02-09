0a4a4bb6019289f1a96add64e57b4d443611e03e
Manikandan Ram <85827977+manikandanramdevops@users.noreply.github.com>
2023-02-09 19:00:25 +0530

    Create iamRoleAttach.py

---
+++ iamRoleAttach.py
@@ -0,0 +1,67 @@
+import json
+import boto3
+
+client = boto3.client("ec2")
+RoleARN="arn:aws:iam::aid:role/s3FullAccessEC2"
+
+
+def lambda_handler(event, context):
+    try:
+        iam_policy = get_instance_iam_policy(event['instance-id'])
+        print(iam_policy['Arn'])
+        if iam_policy is None or iam_policy['Arn'] != iam_policy['Arn']:
+            response = client.associate_iam_instance_profile(
+                IamInstanceProfile={
+                    'Arn': RoleARN,
+                },
+                InstanceId=event['instance-id']
+                )
+            print(f"Successfully attached the IAM policy {RoleARN} with the instance {event['instance-id']}")
+        else:
+            detach_instance_iam_policy(event['instance-id'])
+            attachIAMRole(event['instance-id'])
+    except Exception as e:
+        raise e
+    
+def get_instance_iam_policy(instance_id):
+    response = client.describe_iam_instance_profile_associations(
+        Filters=[
+            {
+                'Name': 'instance-id',
+                'Values': [instance_id]
+            }
+        ]
+    )
+
+    if len(response['IamInstanceProfileAssociations']) > 0:
+        return response['IamInstanceProfileAssociations'][0]['IamInstanceProfile']
+    else:
+        return None
+
+def detach_instance_iam_policy(instance_id):
+    response = client.describe_iam_instance_profile_associations(
+        Filters=[
+            {
+                'Name': 'instance-id',
+                'Values': [instance_id]
+            }
+        ]
+    )
+
+    if len(response['IamInstanceProfileAssociations']) > 0:
+        association_id = response['IamInstanceProfileAssociations'][0]['AssociationId']
+        client.disassociate_iam_instance_profile(AssociationId=association_id)
+        print(f"Successfully detached IAM policy from instance {instance_id}")
+    else:
+        print(f"Instance {instance_id} is not associated with any IAM policy")
+
+
+def attachIAMRole(instance_id):
+    response = client.associate_iam_instance_profile(
+                IamInstanceProfile={
+                    'Arn': RoleARN,
+                },
+                InstanceId=instance_id
+                )
+    print(f"Successfully attached the IAM policy {RoleARN} with the instance {instance_id}")
+    return response
