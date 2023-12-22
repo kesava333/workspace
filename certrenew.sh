1e34d2fbbe2ebb114bf9d41ecc00b35b1758bb29
Kesava Ginjupalli <30998352+kesava333@users.noreply.github.com>
2023-12-22 11:00:33 -0500

    Update certrenew.sh

--- certrenew.sh
+++ certrenew.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
 
-/usr/bin/aws s3 cp s3://otto-certs/aws.ottomotors.com/privkey.pem  /etc/ssl/private/aws.ottomotors.com.key
-/usr/bin/aws s3 cp s3://otto-certs/aws.ottomotors.com/cert.pem /etc/ssl/certs/aws.ottomotors.pem
-
-/etc/init.d/nginx restart
